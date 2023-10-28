dfe528d608eb78c1a82a83b3cac894f47c8008e7
Joel Halkey <joelhalkey@gmail.com>
2023-10-28 17:00:20 -0400

    5pm 10/28/23

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 //Importing the car data from usedCars.js
-import  {usedCars}  from './usedCars.js';
+import { usedCars } from './usedCars.js';
 
 //Non-changing list of all the cars that are in the usedCars.js file
 const carObjects = [];
@@ -18,20 +18,34 @@ for (const carData of usedCars) {
   carObjects.push(car);
 }
 
-console.log(carObjects);
+let displayedCarObjects = [];
 
+//SEARCH
+const searchButton = document.querySelector(".search-button");
+searchButton.addEventListener("click", function () {
+  const display = document.querySelector(".car-main-display");
+  display.innerHTML = "";
+  // Removing car objects from array
+  while (displayedCarObjects.length > 0) {
+    displayedCarObjects.pop();
+  }
+});
 
 /*This function is called when the submit button for the 
 filter is pressed.*/
-function submit(){
-
+function submit() {
 }
 
 
-function filter(){
+function filter() {
 
 }
 
-function display(){
+function createPanel(carObject) {
+  let html = '<div>${carObject.make} ${carObject.model}</div>';
+}
+
+function display(carObjectArray) {
+  const panelContainer = document.querySelector(".car-main-display");
 
 } 
